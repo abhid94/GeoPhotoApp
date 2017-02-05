01a5a2a9ce215e33382bd8ac5295837f55a4be89
Lalitha Katupitiya <lalitha.katupitiya@team.telstra.com>
2017-02-06 09:43:16 +1100

    code clean up and variable naming

--- app_005/FirstViewController.swift
+++ app_005/FirstViewController.swift
@@ -13,9 +13,7 @@ import Parse
 
 class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    
     @IBOutlet var imageView1: UIImageView!
-    
     
     @IBAction func loadButton(_ sender: UIButton) {
         imagePicker.allowsEditing = false
@@ -25,7 +23,6 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
     }
     
     let imagePicker = UIImagePickerController()
-    
     
     @IBAction func loadImageButtonTapped(sender: UIButton) {
         imagePicker.allowsEditing = false
@@ -40,36 +37,20 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
         imagePicker.delegate = self
     }
     
-    // MARK: - UIImagePickerControllerDelegate Methods
-    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView1.contentMode = .scaleAspectFit
             imageView1.image = pickedImage
-            let imageData = UIImagePNGRepresentation(pickedImage)
-            let imageFile = PFFile(name:"pickedImage2.png", data:imageData!)
-            
-            let newPic = PFObject(className:"newPic")
-            newPic["Picture"] = imageFile
-            newPic.saveInBackground()
-            
-            /*
-            PFUser.logInWithUsername(inBackground: "Lalitha_6", password: "hello its m3")
             
             let imageData = UIImagePNGRepresentation(pickedImage)
-            let imageFile = PFFile(name:"pickedImage2.png", data:imageData!)
+            let imageFile = PFFile(name:"imageFile.png", data:imageData!)
             
-            let currUser = PFUser.current()
-            currUser?["displayPicture"] = imageFile
-            currUser?.saveInBackground()*/
-            /*let userPhoto = PFObject(className:"User")
-            userPhoto["imageName"] = "My trip to Hawaii!"
-            userPhoto["imageFile"] = imageFile
-            userPhoto.saveInBackground()*/
+            let GeoPhoto = PFObject(className:"GeoPhoto")
+            GeoPhoto["imageFile"] = imageFile
+            GeoPhoto.saveInBackground()
         } else {
             print("something, went wrong")
         }
-        
         self.dismiss(animated: true, completion: nil)
     }
     
--- app_005/SecondViewController.swift
+++ app_005/SecondViewController.swift
@@ -7,27 +7,18 @@
 //
 
 import UIKit
-import Parse
 
 class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let user = PFUser()
-        user.username = "Lalitha_6"
-        user.password = "hello its m3"
-        user.email = "hello@gmail.com"
-        // other fields can be set just like with PFObject
-        user["phone"] = "696-969-6969"
-        user.signUpInBackground()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
