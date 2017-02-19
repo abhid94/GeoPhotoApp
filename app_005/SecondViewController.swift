18f4c7693ec83e71dc03eb7704d2822690c377f0
Lalitha Katupitiya <lalitha.katupitiya@gmail.com>
2017-02-19 12:37:13 +1100

    Added camera upload functionality

--- app_005/SecondViewController.swift
+++ app_005/SecondViewController.swift
@@ -7,9 +7,34 @@
 //
 
 import UIKit
+import Parse
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
 
+    @IBAction func openCamera(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            let imageData = UIImagePNGRepresentation(pickedImage)
+            let imageFile = PFFile(name:"imageFile.png", data:imageData!)
+            let GeoPhoto = PFObject(className:"GeoPhoto")
+            GeoPhoto["imageFile"] = imageFile
+            GeoPhoto.saveInBackground()
+            print("Should be sent to Parse")
+        } else {
+            print("Something, went wrong")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
