c6c85da089992f6c7a3f3da3f843f76ed98e8c0e
Nirvan <nirry_gelda@hotmail.com>
2017-03-21 21:22:30 +1100

    Change caption font, added caption background, changed up and down buttons

--- app_005/ObjectsTableViewController.swift
+++ app_005/ObjectsTableViewController.swift
@@ -168,8 +168,10 @@ class ObjectsTableViewController: PFQueryTableViewController, CLLocationManagerD
                           completion: nil)
         cell.upvoteCounter.setTitle("\(upvoteCount!)",for: .normal)
         cell.cellImageView.loadInBackground()
-        cell.captionLabel.text = object?.object(forKey: "caption") as? String
-        cell.captionLabel.center = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height*0.6)
+        var newString = object?.object(forKey: "caption") as? String
+        newString = newString!.uppercased()
+        cell.captionLabel.text = newString
+        //cell.captionLabel.center = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height*0.6)
         
         return cell
         
