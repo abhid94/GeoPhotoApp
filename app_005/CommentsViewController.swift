9fbaf4d05ab1cf1e071e9ee9d70d1e98db114201
Abhi Dutta <abhid94@gmail.com>
2017-04-06 22:45:49 +1000

    Removed 2 unused files, removed unused VC, cleaned up code

--- app_005/CommentsViewController.swift
+++ app_005/CommentsViewController.swift
@@ -22,14 +22,11 @@ class CommentsViewController: UIViewController, UITextFieldDelegate, UITableView
             return
         }
         let thisComment = self.commentTextField.text
-        print("done comment box")
-        print(self.titleString)
         let query = PFQuery(className: "GeoPhoto")
-        print(self.titleString)
         query.getObjectInBackground(withId: self.titleString) {
             (object, error) -> Void in
             if error != nil {
-                print("update comment no good")
+
             } else {
                 if let object = object {
                     if(object["comments"] == nil){
@@ -56,14 +53,10 @@ class CommentsViewController: UIViewController, UITextFieldDelegate, UITableView
             return
         }
         let thisComment = self.commentTextField.text
-        print("done comment box")
-        print(self.titleString)
         let query = PFQuery(className: "GeoPhoto")
-        print(self.titleString)
         query.getObjectInBackground(withId: self.titleString) {
             (object, error) -> Void in
             if error != nil {
-                print("update comment no good")
             } else {
                 if let object = object {
                     if(object["comments"] == nil){
@@ -94,24 +87,12 @@ class CommentsViewController: UIViewController, UITextFieldDelegate, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         self.commentTextField.delegate = self
-        //self.textField.delegate = self;
-        //self.navigationController..
         
-        //let backgroundImg = UIImageView(image: UIImage(imageLiteralResourceName: "w-4248"))
-        //self.tableView.backgroundView = backgroundImg
-        //self.mainView.backgroundColor =  UIColor(patternImage: UIImage(imageLiteralResourceName: "w-3829"))
         self.tableView.backgroundColor = UIColor.clear
-        //backgroundImg.makeBlurImage(targetImageView: backgroundImg)
-        
-        
+
         self.title = self.titleString
-        print(self.commentsArray.count)
-        //self.tableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
-        //print(self.commentsArray[0])
+
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(CaptionViewController.dismissKeyboard))
-        
-        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
-        //tap.cancelsTouchesInView = false
         
         view.addGestureRecognizer(tap)
         let recognizer: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeRight))
@@ -125,8 +106,7 @@ class CommentsViewController: UIViewController, UITextFieldDelegate, UITableView
         self.tableView.dataSource = self
         self.sendButton.layer.cornerRadius = 5
         self.tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: self.tableView.frame.size.width, height: 2))
-        
-        //self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
 
     }
     
@@ -151,34 +131,26 @@ class CommentsViewController: UIViewController, UITextFieldDelegate, UITableView
     
     
     func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+
         view.endEditing(true)
     }
     
     
-    func sendButtonClicked () {
-        print("send button pressed")
-    }
     
     func swipeRight(recognizer : UISwipeGestureRecognizer) {
-        //DispatchQueue.main.async(){
-        //self.loadingAnimation()
+
         self.performSegue(withIdentifier: "unwindToFeed2", sender: self)
-        //    self.performSegue(withIdentifier: "segueBackFromComments", sender: self)
-        //}
+
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCellController", for: indexPath) as? CommentCellController
         
-        // Configure the cell...
         cell?.commentLabel2.text = self.commentsArray[indexPath.row]
-        print("LABELZ")
-        print(cell?.commentLabel2.text)
         cell?.commentLabel2.textColor = UIColor.white
         cell?.selectionStyle = UITableViewCellSelectionStyle.none
         cell?.backgroundColor = UIColor.clear
@@ -187,23 +159,13 @@ class CommentsViewController: UIViewController, UITextFieldDelegate, UITableView
     
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: IndexPath) {
-        // cell selected code here
+
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        //self.commentLabel.text = self.commentsArray[0]
+
         return self.commentsArray.count
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
